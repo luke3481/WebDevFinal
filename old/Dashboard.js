b94e4cf55b816b3528b2a4af724e755ed88affb9
sydneyreyrey <92216740+sydneyreyrey@users.noreply.github.com>
2022-05-10 14:27:52 -0600

    found old stuff

---
+++ old/Dashboard.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("courses_sidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("courses_sidenav").style.width = "0";
+}
