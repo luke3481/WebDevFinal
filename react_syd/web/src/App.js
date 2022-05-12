36d29d8382b424d8ac9f3c65f045acd0887a0a3f
918-Spyder <97589040+918-Spyder@users.noreply.github.com>
2022-05-12 08:34:50 -0500

    Replaced functions with classes to utilize states

--- react_syd/web/src/App.js
+++ react_syd/web/src/App.js
@@ -1,42 +1,63 @@
+import React from "react";
 import ClassTile from "./components/ClassTile";
 import SideBar from "./components/SideBar";
 import Assignments from "./components/Assignments";
 
-
-
-function App() {
-  const openNav = () => {
-    console.log("openNav");
-  }
-  const closeNav = () => {
-    console.log("openNav");
-  }
-  return (
-    <div className="App">
-      <div className="container">
-        
-        <SideBar/>
-
-        <div id="classes">
-          <div className="class">
-                <p>Dashboard</p>
-                
-              </div>
-              <ClassTile name={"BUSN 36109 81,02,01 Advanced Decision Models with Python"} color={"rgb(152, 108, 22)"}/>
-              <ClassTile name={"MPCS 52553 1 Web Development"} color={"darkblue"}/>
-              <ClassTile name={"MPCS 55001 1 (Spring 2022) Algorithms"} color={"darkcyan"}/>
-              <ClassTile name={"MPCS 55009 1 (Spring 2022) WebDev"} color={"black"}/>
-
-
-              </div>
-
-          
-          <Assignments Ass1={'assignment1'}/>
-          <Assignments Ass2={'assignment1'}/>
-          <Assignments Ass3={'assignment1'}/>
-        </div>
-      </div>
-  );
-}
+// Replaced function with class to utilize states - 5/11, Seojoon
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: "Home",
+            course_names: ["Course 1", "Course 2", "Course 3"]
+        };
+        this.handler = this.handler.bind(this);
+    }
+    
+    handler() {
+        this.setState({
+            mode: "Course"
+        });
+    }
+    
+    openNav() {
+      console.log("openNav");
+    }
+    
+    closeNav() {
+      console.log("openNav");
+    }
+    
+    render() {
+        if (this.state.mode == "Home") {
+            return (
+              <div className="App">
+                <div className="container">
+                  
+                  <SideBar action = {this.handler} state = {this.state} />
+            
+                  <div id="classes">
+                    <div className="class">
+                          <p>Dashboard</p>
+                          
+                        </div>
+                        <ClassTile name={"BUSN 36109 81,02,01 Advanced Decision Models with Python"} color={"rgb(152, 108, 22)"}/>
+                        <ClassTile name={"MPCS 52553 1 Web Development"} color={"darkblue"}/>
+                        <ClassTile name={"MPCS 55001 1 (Spring 2022) Algorithms"} color={"darkcyan"}/>
+                        <ClassTile name={"MPCS 55009 1 (Spring 2022) WebDev"} color={"black"}/>
+            
+            
+                        </div>
+            
+                    
+                    <Assignments Ass1={'assignment1'}/>
+                    <Assignments Ass2={'assignment1'}/>
+                    <Assignments Ass3={'assignment1'}/>
+                  </div>
+                </div>
+            );
+        }
+    }
+} 
 
 export default App;
--- react_syd/web/src/components/SideBar.js
+++ react_syd/web/src/components/SideBar.js
@@ -1,38 +1,44 @@
-
-
-
-function SideBar(props) {
-    
-    const openNav = () => {
+import React from "react";
+ 
+class SideBar extends React.Component {  
+    openNav() {
         document.getElementById("courses_sidenav").style.width = "250px";
       }
     
-    const closeNav = () => {
+    closeNav() {
         console.log('hey')
         //document.getElementById("courses_sidenav").style.width = "0";
     }
+    
+    renderCourse(i) {
+        return (
+            <a href="" onClick={this.props.action}>
+                {this.props.state.course_names[i]}
+            </a>
+        );
+    }
 
-
-    return(
-        <div id="sidebar">
-            <div id="sidebar_button">
-                <button type="button" id="btnstyle1" ><img id="home" src="https://instructure-uploads.s3.amazonaws.com/account_23490000000000001/attachments/94286/shield.cmyk.reversed.png?AWSAccessKeyId=AKIAJFNFXH2V2O7RPCAA&Expires=1941800704&Signature=UMFgyK223WwtXIfmSeqYTDFxDNY%3D&response-cache-control=Cache-Control%3Amax-age%3D473364000.0%2C%20public&response-expires=473364000.0"/></button>
-                <button type="button" id="btnstyle2" ><img src={require("../imgs/account.jpg")}/>Account</button>
-                <button type="button" id="btnstyle2" ><img src={require("../imgs/dashboard.png")}/>Dashboard</button>
-                <button type="button" id="btnstyle2" onClick={() => openNav()}><img src={require("../imgs/courses.png")}/>Courses</button>
-                <div id="courses_sidenav" className="sidenav">
-                <a href="" className="closebtn" onClick={closeNav()}>&times;</a>
-                <h3>Courses</h3>
-                <a href="">Course1</a>
-                <a href="">Course2</a>
-                <a href="">Course3</a>
+    render() {
+        return(
+            <div id="sidebar">
+                <div id="sidebar_button">
+                    <button type="button" id="btnstyle1" ><img id="home" src="https://instructure-uploads.s3.amazonaws.com/account_23490000000000001/attachments/94286/shield.cmyk.reversed.png?AWSAccessKeyId=AKIAJFNFXH2V2O7RPCAA&Expires=1941800704&Signature=UMFgyK223WwtXIfmSeqYTDFxDNY%3D&response-cache-control=Cache-Control%3Amax-age%3D473364000.0%2C%20public&response-expires=473364000.0"/></button>
+                    <button type="button" id="btnstyle2" ><img src={require("../imgs/account.jpg")}/>Account</button>
+                    <button type="button" id="btnstyle2" ><img src={require("../imgs/dashboard.png")}/>Dashboard</button>
+                    <button type="button" id="btnstyle2" onClick={() => this.openNav()}><img src={require("../imgs/courses.png")}/>Courses</button>
+                    <div id="courses_sidenav" className="sidenav">
+                        <a href="" className="closebtn" onClick={this.closeNav()}>&times;</a>
+                        <h3>Courses</h3>
+                        {this.renderCourse(0)}
+                        {this.renderCourse(1)}
+                        {this.renderCourse(2)}
+                    </div>
+                    <button type="button" id="btnstyle2" ><img src={require("../imgs/settings.png")}/>Settings</button>
+                    <button type="button" id="btnstyle2" ><img src={require("../imgs/logout.png")}/>Logout</button>
                 </div>
-                <button type="button" id="btnstyle2" ><img src={require("../imgs/settings.png")}/>Settings</button>
-                <button type="button" id="btnstyle2" ><img src={require("../imgs/logout.png")}/>Logout</button>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
 
 export default SideBar;
