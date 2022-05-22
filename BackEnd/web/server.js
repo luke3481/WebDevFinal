59ed9d83644fc490167f3d3aad42c63af3fbfa12
sydneyreyrey <sydreynolds@chicagobooth.edu>
2022-05-21 19:44:41 -0500

    Testing a fake database

--- BackEnd/web/server.js
+++ BackEnd/web/server.js
@@ -1,6 +1,20 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express()
+const bodyParser = require("body-parser");
+const logger = require ("morgan");
+
+
+
+
+const app = express();
+
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
 
 app.use(cors());
 
